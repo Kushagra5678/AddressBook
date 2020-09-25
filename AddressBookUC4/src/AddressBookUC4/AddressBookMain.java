a5989e758dc85b3d8401dfff3cf7a158621b6139
Kushagra5678 <kushu.7feb@gmail.com>
2020-09-25 22:11:13 +0530

    UC4 commit

---
+++ AddressBookUC4/src/AddressBookUC4/AddressBookMain.java
@@ -0,0 +1,29 @@
+package AddressBookUC4;
+import java.util.Scanner;
+
+public class AddressBookMain {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		 Contacts c = new Contacts("Kush","Agg","Alpha 1 Noida","UP","201302","9891876543","kush@gmail.com");
+		   AddressBook addressBook = new AddressBook();
+		   addressBook.addContact(c);
+		   System.out.println("Enter the Name of the person whose details are to be edited");
+		   String name = sc.nextLine();
+	       String[] s = name.split(" ");
+		   System.out.println("Enter the new Address,State,zip,phone number,email of the person");
+		   String address = sc.nextLine();
+		   String state = sc.nextLine();
+		   String zip = sc.nextLine();
+		   String phNo = sc.nextLine();
+		   String email = sc.nextLine();
+	       addressBook.editContact(s[0], s[1], address, state, zip, phNo, email);
+	       System.out.println("Details Edited Succesfully!!!!!!");
+	       System.out.println("Enter the name of the person whose details are to be deleted");
+		   name=sc.nextLine();
+		   String[] x = name.split(" ");
+		   addressBook.delContact(x[0], x[1]);
+		   System.out.println("The person's details are deleted Succesfully!!!!!!"); 
+
+	}
+}
